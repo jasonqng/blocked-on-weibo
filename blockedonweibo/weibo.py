a39d941b76e2a382a8d1f9340aee4e7bf3319eaa
Jason Ng <jason.q.ng@gmail.com>
2017-09-24 22:15:48 -0400

    Handle lists, fix sql query, rewrite functions

--- blockedonweibo/weibo.py
+++ blockedonweibo/weibo.py
@@ -14,6 +14,9 @@ import io
 import requests
 import base64  
 import re
+import ast
+from df2gspread import gspread2df as g2d
+import sys
 try:
     from urllib.parse import urlparse
 except ImportError:
@@ -21,85 +24,87 @@ except ImportError:
 import rsa  
 import json  
 import binascii  
+import math
 from . import weibo_credentials
 ### SETTINGS
 
-censorship_phrase_utf8 = '根据相关法律法规和政策'
-censorship_phrase_decoded = codecs.encode(censorship_phrase_utf8.decode('utf8'), 'unicode_escape')
+CENSORSHIP_PHRASE_UTF8 = '根据相关法律法规和政策'
+CENSORSHIP_PHRASE_DECODED = codecs.encode(CENSORSHIP_PHRASE_UTF8.decode('utf8'), 'unicode_escape')
 
-captcha_phrase_utf8 = '你的行为有些异常'
-captcha_phrase_decoded = codecs.encode(captcha_phrase_utf8.decode('utf8'), 'unicode_escape')
+CAPTCHA_PHRASE_UTF8 = '你的行为有些异常'
+CAPTCHA_PHRASE_DECODED = codecs.encode(CAPTCHA_PHRASE_UTF8.decode('utf8'), 'unicode_escape')
 
-no_results_phrase_utf8 = '抱歉，未找到'
-no_results_phrase_decoded = codecs.encode(no_results_phrase_utf8.decode('utf8'), 'unicode_escape')
+NO_RESULTS_PHRASE_UTF8 = '抱歉，未找到'
+NO_RESULTS_PHRASE_DECODED = codecs.encode(NO_RESULTS_PHRASE_UTF8.decode('utf8'), 'unicode_escape')
 
-class Userlogin:  
+def user_login(username=weibo_credentials.Creds().username,
+                password=weibo_credentials.Creds().password,
+                write_cookie=True):
     """
     Logs a user into Weibo and generates a cookie
     Returns a Requests session with cookie object
     Pulls username and password from weibo_credentials.py
     code from https://www.zhihu.com/question/29666539 with minor modifications
     """
-    def userlogin(self,username,password,write_cookie=True):  
-        session = requests.Session()  
-        url_prelogin = 'http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=&rsakt=mod&client=ssologin.js(v1.4.5)&_=1364875106625'  
-        url_login = 'http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.5)'  
-  
-        #get servertime,nonce, pubkey,rsakv  
-        resp = session.get(url_prelogin)  
-        json_data  = re.findall(r'(?<=\().*(?=\))', resp.text)[0]
-        data       = json.loads(json_data)  
+    session = requests.Session()  
+    url_prelogin = 'http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=&rsakt=mod&client=ssologin.js(v1.4.5)&_=1364875106625'  
+    url_login = 'http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.5)'  
+
+    #get servertime,nonce, pubkey,rsakv  
+    resp       = session.get(url_prelogin)  
+    json_data  = re.findall(r'(?<=\().*(?=\))', resp.text)[0]
+    data       = json.loads(json_data)  
 
 
-        servertime = data['servertime']  
-        nonce      = data['nonce']  
-        pubkey     = data['pubkey']  
-        rsakv      = data['rsakv']  
-  
-        # calculate su  
-        su  = base64.b64encode(username.encode(encoding="utf-8"))  
-  
-        #calculate sp  
-        rsaPublickey= int(pubkey,16)  
-        key = rsa.PublicKey(rsaPublickey,65537)  
-        message = str(servertime) +'\t' + str(nonce) + '\n' + str(password)  
-        sp = binascii.b2a_hex(rsa.encrypt(message.encode(encoding="utf-8"),key))  
-        postdata = {  
-                            'entry': 'weibo',  
-                            'gateway': '1',  
-                            'from': '',  
-                            'savestate': '7',  
-                            'userticket': '1',  
-                            'ssosimplelogin': '1',  
-                            'vsnf': '1',  
-                            'vsnval': '',  
-                            'su': su,  
-                            'service': 'miniblog',  
-                            'servertime': servertime,  
-                            'nonce': nonce,  
-                            'pwencode': 'rsa2',  
-                            'sp': sp,  
-                            'encoding': 'UTF-8',  
-                            'url': 'http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',  
-                            'returntype': 'META',  
-                            'rsakv' : rsakv,  
-                            }  
-        resp = session.post(url_login,data=postdata)  
-        # print resp.headers 
-        #print(resp.content)
-        login_url = re.findall(r'http://weibo.*&retcode=0',resp.text)  
-        #print(login_url)
-        respo = session.get(login_url[0])  
-        uid = re.findall('"uniqueid":"(\d+)",',respo.text)[0]  
-        url = "http://weibo.com/u/"+uid  
-        respo = session.get(url)
-        if write_cookie:
-            cookie_dict = session.cookies.get_dict()
-            with open(username + "_cookie.txt", 'w') as f:
-                f.write(cookie)
-        return session
+    servertime = data['servertime']  
+    nonce      = data['nonce']  
+    pubkey     = data['pubkey']  
+    rsakv      = data['rsakv']  
 
-def has_censorship(keyword,cookies=None):
+    # calculate su  
+    su  = base64.b64encode(username.encode(encoding="utf-8"))  
+
+    #calculate sp  
+    rsaPublickey= int(pubkey,16)  
+    key = rsa.PublicKey(rsaPublickey,65537)  
+    message = str(servertime) +'\t' + str(nonce) + '\n' + str(password)  
+    sp = binascii.b2a_hex(rsa.encrypt(message.encode(encoding="utf-8"),key))  
+    postdata = {  
+                        'entry': 'weibo',  
+                        'gateway': '1',  
+                        'from': '',  
+                        'savestate': '7',  
+                        'userticket': '1',  
+                        'ssosimplelogin': '1',  
+                        'vsnf': '1',  
+                        'vsnval': '',  
+                        'su': su,  
+                        'service': 'miniblog',  
+                        'servertime': servertime,  
+                        'nonce': nonce,  
+                        'pwencode': 'rsa2',  
+                        'sp': sp,  
+                        'encoding': 'UTF-8',  
+                        'url': 'http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',  
+                        'returntype': 'META',  
+                        'rsakv' : rsakv,  
+                        }  
+    resp = session.post(url_login,data=postdata)  
+    # print resp.headers 
+    #print(resp.content)
+    login_url = re.findall(r'http://weibo.*&retcode=0',resp.text)  
+    #print(login_url)
+    respo = session.get(login_url[0])  
+    uid = re.findall('"uniqueid":"(\d+)",',respo.text)[0]  
+    url = "http://weibo.com/u/"+uid  
+    respo = session.get(url)
+    if write_cookie:
+        cookie_dict = session.cookies.get_dict()
+        json.dump(cookie_dict, open(username + "_cookie.txt",'w'))
+    return session
+
+def has_censorship(keyword,
+                cookies=None):
     """
     Function which actually looks up whether a search for the given keyword returns text
     which is displayed during censorship.
@@ -117,15 +122,18 @@ def has_censorship(keyword,cookies=None):
         r = requests.get(url,cookies=cookies).text
         i = 1
         while True:
-            if captcha_phrase_decoded not in r:
+            if CAPTCHA_PHRASE_DECODED not in r:
                 break
             else:
-                print "CAPTCHA", keyword
+                print("CAPTCHA", keyword, "sleeping for %s seconds" % 300*i)
                 time.sleep(300*i)
                 i+=1
+            if i==50:
+                print("Can't break out of CAPTCHA, aborting")
+                sys.exit(1)
     except IOError:
         wait_seconds = random.randint(90, 100)
-        print "connection reset, waiting %s" % wait_seconds
+        print("connection reset, waiting %s" % wait_seconds)
         time.sleep(wait_seconds)
         return "reset"
 
@@ -135,29 +143,36 @@ def has_censorship(keyword,cookies=None):
     else:
         None
     
-    if censorship_phrase_decoded in r:
+    if CENSORSHIP_PHRASE_DECODED in r:
         return ("censored",None)
-    elif no_results_phrase_decoded in r:
+    elif NO_RESULTS_PHRASE_DECODED in r:
         return ("no_results",None)
     else:
         return ("has_results",num_results)
 
-def create_table(sqlite_file):
+def create_table(sqlite_file, overwrite=False):
     """
     Generating a sqlite file for storing results
     Multi-index primary key on id, date, and source
     Set new_database to True in order to remove any existing file
     """
-    conn = sqlite3.connect(sqlite_file)
-    c = conn.cursor()
-    c.execute('CREATE TABLE results (id int, date date, datetime datetime, keyword string, censored bool, no_results bool, reset bool, result string, source string, num_results int, notes string, PRIMARY KEY(id,date,source))')
-    conn.commit()
-    conn.close()
-    
+    if overwrite and os.path.isfile(sqlite_file):
+        os.remove(sqlite_file)
+    if not os.path.isfile(sqlite_file):
+        conn = sqlite3.connect(sqlite_file)
+        c = conn.cursor()
+        c.execute('CREATE TABLE results (id int, date date, datetime datetime, keyword string, censored bool, no_results bool, reset bool, result string, source string, num_results int, notes string, PRIMARY KEY(id,date,source))')
+        conn.commit()
+        conn.close()
 
-    
 
-def insert_into_table(record_id,keyword,result,source,notes=None,num_results=None,sqlite_file=None):
+def insert_into_table(record_id,
+                keyword,
+                result,
+                source=None,
+                num_results=None,
+                notes=None,
+                sqlite_file=None):
     """
     Writing the results to the sqlite database file
     """
@@ -191,7 +206,9 @@ def insert_into_table(record_id,keyword,result,source,notes=None,num_results=Non
     conn.commit()
     conn.close()
 
-def get_keywords_from_source(location,keyword_col_name,source_name,lxb_categories=None):
+def get_keywords_from_source(location,
+                keyword_col_name,
+                source_name):
     """
     Generating the keyword lists to search on
     """
@@ -203,9 +220,6 @@ def get_keywords_from_source(location,keyword_col_name,source_name,lxb_categorie
         pass
     else:
         test_df = g2d.download(location,wks_name='Sheet2',col_names=True)
-    if lxb_categories:
-        mask = test_df.category.isin(lxb_categories)
-        test_df = test_df[mask]
     if '.csv' in location:
         test_keywords['category'] = test_df.category
     test_keywords['keyword'] = test_df[keyword_col_name]
@@ -213,12 +227,14 @@ def get_keywords_from_source(location,keyword_col_name,source_name,lxb_categorie
     test_keywords['notes'] = None
     return test_keywords
     
-def sqlite_to_df(sqlite_file):
+def sqlite_to_df(sqlite_file,
+                query="select * from results where source!='_meta_' or source is NULL;"):
     conn = sqlite3.connect(sqlite_file)
-    df = pd.read_sql_query("select * from results where source!='_meta_';", conn)
+    df = pd.read_sql_query(query, conn)
     return df
 
-def verify_cookies_work(cookie,return_full_response=False):
+def verify_cookies_work(cookie,
+                return_full_response=False):
     """
     Returns True if cookies return profile indicator
     If no cookie or bad cookie is passed, you get a generic login page which doesn't have the indicator
@@ -232,27 +248,55 @@ def verify_cookies_work(cookie,return_full_response=False):
     else:
         return False
 
-def run(test_keywords,verbose='some',insert=True,sqlite_file=None,return_df=False,sleep=True,cookies=None):
+def load_cookies(cookie_file=weibo_credentials.Creds().username + "_cookie.txt"):
+    with open(cookie_file, 'r') as f:
+        #cookie = ast.literal_eval(f.read())
+        cookie = json.load(f)
+    return cookie
+
+def run(test_keywords,
+                verbose='some',
+                insert=True,
+                sqlite_file=None,
+                return_df=False,
+                sleep=True,
+                cookies=None,
+                sleep_secs=15,
+                continue_interruptions=True):
     """
     Iterating through the keyword list and testing one at a time
     Handles when script or connection breaks; will pick up where it left off
     Set return_df to append each new result to a df in memory, which is returned at end of function
     verbose = 'some','all',or 'none'(technically anything besides 'some' or 'all' will not show anything)
+    sleep = time in seconds to sleep between searches
     """
+    if sqlite_file:
+        create_table(sqlite_file)
+
     count=0
     if return_df:
         results_df = pd.DataFrame()
-    
+
+    if isinstance(test_keywords, list):
+        test_keywords = pd.DataFrame(test_keywords,columns=["keyword"])
+
+    if "Index" not in test_keywords.columns:
+        test_keywords['Index'] = test_keywords.index
+    if "notes" not in test_keywords.columns:
+        test_keywords['notes'] = None
+    if "source" not in test_keywords.columns:
+        test_keywords['source'] = None        
+
     for r in test_keywords.itertuples():
-        if insert and r.Index < len(sqlite_to_df(sqlite_file)):
+        if insert and r.Index < len(sqlite_to_df(sqlite_file)) and continue_interruptions:
             continue
         result,num_results = has_censorship(r.keyword,cookies)
         if verbose=="all":
-            print r.Index,r.keyword, result
+            print(r.Index,r.keyword, result)
         elif verbose=="some" and (count%10==0 or count==0):
-            print r.Index,r.keyword, result
+            print(r.Index,r.keyword, result)
         if insert:
-            insert_into_table(len(sqlite_to_df(sqlite_file)),r.keyword,result,r.source,num_results,r.notes,sqlite_file=sqlite_file)
+            insert_into_table(len(sqlite_to_df(sqlite_file)),r.keyword,result=result,source=r.source,num_results=num_results,notes=r.notes,sqlite_file=sqlite_file)
         if return_df:
             results_df = pd.concat([results_df,
                                     pd.DataFrame([{"date":datetime.now().date(),
@@ -265,9 +309,9 @@ def run(test_keywords,verbose='some',insert=True,sqlite_file=None,return_df=Fals
                                    ])
         count+=1
         if sleep:
-            time.sleep(random.randint(13, 16))
+            time.sleep(random.randint(math.ceil(sleep_secs*.90), math.ceil(sleep_secs*1.10)))
     if insert:
-        insert_into_table(int(test_keywords.index.max())+1,None,"finished","_meta_",sqlite_file=sqlite_file)
+        insert_into_table(int(test_keywords.index.max())+1,None,result="finished",source="_meta_",sqlite_file=sqlite_file)
     if return_df:
         return results_df
 
