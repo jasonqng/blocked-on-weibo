f020bc5e47072988477d5782f78f133918687c79
ruohan <ruohan@citizenlab.ca>
2017-11-10 14:41:01 -0500

    increment to version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='blockedonweibo',
-      version='0.1',
+      version='0.2',
       description='Testing if search keywords are censored on Sina Weibo',
       url='http://github.com/jasonqng/blocked-on-weibo',
       author='Jason Q. Ng',
