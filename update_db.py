6588cb1c5b81c6d03fbce37e1834455f0554dbff
ruohan <ruohan@citizenlab.ca>
2017-11-16 02:13:33 -0500

    finalizes the db migrate/update script

--- update_db.py
+++ update_db.py
@@ -8,7 +8,7 @@ def find_db():
     db_files = [file for file in files if ".db" in file or ".sqlite" in file]
     if len(db_files)>0:
         if len(db_files) == 1:
-            user_input = input("Is database file %s/%s? (y/N) " %(os.getcwd(), db_files[0]))
+            user_input = raw_input("Is database file %s/%s? (y/N) " %(os.getcwd(), db_files[0]))
             if user_input.lower() == 'y':
                 migrate_db(db_files[0])
             else:
@@ -18,7 +18,7 @@ def find_db():
             for i in range(len(db_files)):
                 print("%d. %s" %(i+1, db_files[i]))
             print("**************")
-            user_input = input("Which db file to upgrade? (Enter NUMBER) ")
+            user_input = raw_input("Which db file to upgrade? (Enter NUMBER) ")
             if user_input.isdigit() and 0 < int(user_input) < len(db_files):
                 migrate_db(db_files[int(user_input) - 1])
             else:
@@ -27,13 +27,14 @@ def find_db():
     else:
         break_script()
 
+
 def migrate_db(db_location):
     conn = sqlite3.connect(db_location)
     c = conn.cursor()
     c.execute('ALTER TABLE results RENAME TO resultsOld')
     c.execute('''CREATE TABLE results (id INT, date DATE, datetime_logged DATETIME, test_number INT, keyword string, 
-        censored bool, no_results bool, reset bool, is_min bool, result string, source string, orig_keyword string, 
-        num_results INT, notes string, PRIMARY KEY(date,source,test_number,keyword))''')
+        censored bool, no_results bool, reset bool, is_canonical bool, result string, source string, orig_keyword string, 
+        num_results INT, notes string, PRIMARY KEY(date,source,test_number,keyword,orig_keyword))''')
     c.execute('''INSERT INTO results (id, date, datetime_logged, test_number, keyword, censored, no_results, reset,
         result, source, num_results, notes)
                 SELECT id, date, datetime_logged, test_number, keyword, censored, no_results, reset, result, source,
